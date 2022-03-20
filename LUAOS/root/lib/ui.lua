b4e5a91fbb6a11f611c0f5dc6f86aeb9c8c7fd30
bnjmn21 <benma2321@gmail.com>
2022-03-20 17:09:22 +0100

    ok

--- LUAOS/root/lib/ui.lua
+++ LUAOS/root/lib/ui.lua
@@ -1,35 +1,3 @@
---#region class button
----@class button
----@field x number
----@field y number
----@field dx number
----@field dy number
----@field bgc number
----@field txtc number
----@field txt string
----@field onPress function
----@field onRelease function
----@field display table
-local button = {}
-
----@param param table @list of parameters for button
----@return table @the button object
-function button:new(param)
-    local obj = {}
-    setmetatable(obj, self)
-    self.__index = self
-    for i,o in pairs(param) do
-        self[i] = o
-    end
-    return obj
-end
-
----draws the button
-function button:draw()
-
-end
---#endregion
-
 ---Wrapps text to boundraries
 ---@param text string @the string to wrap
 ---@param max number @the maximum characters per line
@@ -80,3 +48,50 @@ local function wrap(text,max,respectWord)
         return wrapped
     end
 end
+
+--#region objects
+---@class button
+---@field id number
+---@field x number
+---@field y number
+---@field dx number
+---@field dy number
+---@field bgc number
+---@field txtc number
+---@field txt string
+---@field display table
+local button = {}
+
+---@param param table @list of parameters for button
+---@return table @the button object
+function button:new(param)
+    local obj = {}
+    setmetatable(obj, self)
+    self.__index = self
+    for i,o in pairs(param) do
+        self[i] = o
+    end
+    return obj
+end
+
+---draws the button
+---@param UI table @the UI
+function button:draw(UI)
+    local wrapped = wrap(self.txt,self.dx)
+    self.display.setBackgroundColor(self.bgc)
+    self.display.setTextColor(self.txtc)
+    for i in pairs(wrapped) do
+        if i > self.dy then break end
+        if self.y+UI.Yscroll+i-1 > UI.dy then break end
+        self.display.setCursorPos(self.x,UI.y+self.y+UI.Yscroll+i-1)
+        self.display.write(string.sub(wrapped[i],1,UI.dx-self.x))
+    end
+end
+
+---
+function button:checkEvent(UI,event)
+    if event[3] >= self.x and event[3] < self.x+self.dx and event[4] >= self.y+UI.Yscroll and event[4] < self.y+self.dy+UI.Yscroll then
+        os.queueEvent("UI_button_down",self.id)
+    end
+end
+--#endregion
